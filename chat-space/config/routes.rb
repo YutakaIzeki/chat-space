0dcaaf35ad19a1af77845823df63869dbef74b3f
Yutaka Izeki <einegitarreund@gmail.com>
2019-03-16 17:01:09 +0900

    Setting routing about groups controller

--- chat-space/config/routes.rb
+++ chat-space/config/routes.rb
@@ -4,12 +4,13 @@ Rails.application.routes.draw do
   root 'messages#index'
 
 
-resources :users, only: [:edit, :update]
+  resources :users, only: [:edit, :update]
   # get 'users/update'
   # get 'users/:id/edit' => "users#edit"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  # get "group/new" => "groups#new"
+  resources :groups, only: [:new, :create, :edit, :update]
 
 end
 
