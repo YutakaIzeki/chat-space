64c43fb4359c357ccb32c6fe7d831ee21afbadcb
Yutaka Izeki <einegitarreund@gmail.com>
2019-03-25 00:41:46 +0900

    add html , done and fail function

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,25 @@
 $(function(){
+  function buildHTML(message){
+    let image = message.image_present ? message.image : "" ;
+    var html = `<div class="message">
+                  <div class="message__upper-info">
+                    <p class="message__upper-info__talker">
+                      ${message.user_name}
+                    </p>
+                    <p class="message__upper-info__date">
+                      ${message.time}
+                    </p>
+                  </div>
+                  <div class="message__lower">
+                      <p class="message__text">
+                        ${message.content}
+                      </p>
+                      <img class="message__lower__image" src="${image}" >
+                      <img class="message__lower__image" src="" >
+                  </div>
+                </div>`
+    return html;
+  }
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -11,6 +32,15 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      var html =buildHTML(data);
+      $('.messages').append(html)
+      $('.input-box__text').val("")
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+    })
+    .fail(function(){
+      alert('error');
     })
   })
 })
