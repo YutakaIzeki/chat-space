50e9dec252a2fe5819259fc5b58b3b550138fbe1
Yutaka Izeki <einegitarreund@gmail.com>
2019-03-25 21:26:24 +0900

    create appendName and appendMsg function

--- app/assets/javascripts/channels/member_search.js
+++ app/assets/javascripts/channels/member_search.js
@@ -2,14 +2,24 @@ $(function(){
 
   var search_list = $('#user-search-result');
 
-  function buildHTML(user){
+  function appendName(user){
 
     var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${user.name}</p>
                   <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="ユーザー名">追加</a>
                 </div>`
-    return html;
+    search_list.append(html);
   }
+
+  function appendMsg(msg){
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${msg}</p>
+                </div>`
+    search_list.append(html);
+  }
+
+
+
   $('#user-search-field.chat-group-form__input').on('keyup', function(){
     var input = $("#user-search-field.chat-group-form__input").val();
     console.log(input);
@@ -24,15 +34,14 @@ $(function(){
       $('#user-search-result').empty();
       if(users.length !== 0) {
         users.forEach((user) => {
-          var html =buildHTML(user);
-          search_list.append(html)
+          appendName(user)
         });
       }else{
-        search_list.append(html)
+        appendMsg("該当するユーザが見つかりません")
       }
     })
     .fail(function(){
-      alert('error');
+      alert('ユーザー検索に失敗しました');
     })
   })
 })
