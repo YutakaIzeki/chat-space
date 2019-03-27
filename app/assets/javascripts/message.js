51feb9acb448a5d3306f04324df17221fffd59f2
Yutaka Izeki <einegitarreund@gmail.com>
2019-03-27 20:05:56 +0900

    Edit message js
    - 追加するメッセージにメッセージidとグループのidを与える
    
    - 自動更新機能の追加

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,7 @@
 $(function(){
   function buildHTML(message){
     let image = message.image_present ? message.image : "" ;
-    var html = `<div class="message">
+    var html = `<div class="message" id="${message.id} ${message.group_id}">
                   <div class="message__upper-info">
                     <p class="message__upper-info__talker">
                       ${message.user_name}
@@ -32,7 +32,7 @@ $(function(){
       contentType: false
     })
     .done(function(data){
-      var html =buildHTML(data);
+      var html = buildHTML(data);
       $('.messages').append(html)
       $('#new_message')[0].reset()
       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
@@ -43,4 +43,34 @@ $(function(){
       $('.submit-btn').prop('disabled', false);
     })
   })
+
+  $(function(){
+    setInterval(update,5000);
+  })
+  function update(){
+    var message = $('.messages .message:last-child').attr('id')
+    // console.log(message)
+    $.ajax({
+      url: location.href,
+      type: 'GET',
+      data: { message: message },
+      dataType: 'json'
+    })
+    .always(function(data){
+      console.log(data)
+      if($.isEmptyObject(data)){
+        // console.log("hello")
+      }else{
+        data.forEach(function(element){
+          // console.log(element)
+          var html = buildHTML(element)
+          $('.messages').append(html)
+        })
+        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+      }
+    })
+  }
+
+
+
 })
